e455e7974bf20dbbf648a105c266a7d9cb5ab75f
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2022-08-03 11:58:58 +0200

    add btn action

--- Documents/Max/pro/Formation/projet/apppMeteo/main.js
+++ Documents/Max/pro/Formation/projet/apppMeteo/main.js
@@ -1,4 +1,8 @@
 console.log('test')
+
+function action() {
+    alert('ok')
+}
 const axi = document.getElementById("met");
 var liste;
 fetch("https://api.openweathermap.org/data/2.5/weather?q=bethune&units=metric&appid=f1d14debaa874f9e1698ca874b570186")
@@ -13,13 +17,13 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=bethune&units=metric&ap
 
 function foo() {axi.innerHTML=`
 <div class="card" style="width: 18rem;">
-<img src=" " class="card-img-top" alt="...">
-<div class="card-body">
- <input type="text" placeholder="recherche">  <button><img src="/apppMeteo/img/search.png" id="searchImg"/></button>
+ <div class="card-body">
+ <input type="text" placeholder="recherche">  <button onclick="action()"><img src="/apppMeteo/img/search.png" id="searchImg"/></button>
 
   <h5 class="card-title">${liste.main.temp}c</h5>
-  <span>${liste.country}</span>
-  <span>${liste.main.humidity}</span>
+  
+  <span>humidity :${liste.main.humidity}%</span><br/>
+  <span>Wind speed :${liste.wind.speed}km</span>
 
 
 </div>
