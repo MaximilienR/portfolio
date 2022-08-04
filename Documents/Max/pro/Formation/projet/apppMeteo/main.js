deca59c780b7ff3f67335761ca65e522e2771810
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2022-08-04 11:21:00 +0200

    update

--- Documents/Max/pro/Formation/projet/apppMeteo/main.js
+++ Documents/Max/pro/Formation/projet/apppMeteo/main.js
@@ -1,27 +1,33 @@
 console.log('test')
+var demande = document.getElementById('demande')
+if (demande!="") {
+    console.log('veuillez saisir du texte')
+}else{console.log(demande)}
 
+var save = document.getElementById("demande");
 function action() {
-    alert('ok')
+    console.log(demande)
 }
+
 const axi = document.getElementById("met");
 var liste;
 fetch("https://api.openweathermap.org/data/2.5/weather?q=bethune&units=metric&appid=f1d14debaa874f9e1698ca874b570186")
 .then(response=>response.json()) 
 .then(data=>{
     liste=data;
-    alert()
+     alert()
     console.log(liste.temp);
-    foo();
+    affiche();
 
 });
 
-function foo() {axi.innerHTML=`
+function affiche() {axi.innerHTML=`
 <div class="card" style="width: 18rem;">
  <div class="card-body">
- <input type="text" placeholder="recherche">  <button onclick="action()"><img src="/apppMeteo/img/search.png" id="searchImg"/></button>
+ <input type="text" placeholder="recherche" id="demande">  <button onclick="action()"><img src="/apppMeteo/img/search.png" id="searchImg"/></button>
 
   <h5 class="card-title">${liste.main.temp}c</h5>
-  
+  <span>${liste.weather.icon}</span><br/>
   <span>humidity :${liste.main.humidity}%</span><br/>
   <span>Wind speed :${liste.wind.speed}km</span>
 
